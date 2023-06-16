06601aecfcf69ab0fe8f295c5aa5eff80b841de7
MuhammadUsaid-github <usaid.dxb.ae@gmail.com>
2023-06-16 04:07:22 -0700

    start

---
+++ app.js
@@ -0,0 +1,24 @@
+// // Get references to the elements
+// const todoInput = document.getElementById('todo-input');
+// const addButton = document.getElementById('add-button');
+// const todoList = document.getElementById('todo-list');
+
+// // Add event listener to the "Add" button
+// addButton.addEventListener('click', addTodo);
+
+// // Function to add a new to-do item
+// function addTodo() {
+//     const todoText = todoInput.value;
+
+//     if (todoText.trim() !== '') {
+//         const todoItem = document.createElement('li');
+//         todoItem.classList.add('todo-item');
+//         todoItem.textContent = todoText;
+
+//         todoList.appendChild(todoItem);
+//         todoInput.value = '';
+//     }
+// }
+// alert("hellow world what are you doing") 
+// var a = prompt("whts up","whts up")
+// alert("mashallah")
